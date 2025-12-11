d4c7aafe5c2795f6c72eaa850e2d8bfc109296ee
rahul8628039-oss <rahul8628039@gmail.com>
2025-12-11 11:02:09 +0530

    Delete creepy-cartoon-yt-bot/scripts directory

--- creepy-cartoon-yt-bot/scripts/make_thumbnail.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image, ImageDraw
-
-img = Image.new("RGB", (1280, 720), "black")
-d = ImageDraw.Draw(img)
-d.text((300, 300), "CREEPY CARTOON", fill="red")
-img.save("outputs/thumbnail.jpg")
